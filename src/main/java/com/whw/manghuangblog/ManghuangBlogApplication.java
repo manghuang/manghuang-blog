c783199a00c58ed760b93fd9ff4b526c24ade60c
lenovo <1>
2020-12-29 21:11:43 +0800

    init

---
+++ src/main/java/com/whw/manghuangblog/ManghuangBlogApplication.java
@@ -0,0 +1,13 @@
+package com.whw.manghuangblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManghuangBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManghuangBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whw/manghuangblog/ManghuangBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whw.manghuangblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManghuangBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
